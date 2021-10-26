10f2dbbca534ec02fc636afabfe08337fa0b7bed
Yuqing Wu <ywu20@stuy.edu>
2021-10-26 14:00:14 -0400

    works

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 //generates a random number and return
 int populate(int * rand){
- int file = open("/dev/random",O_RDONLY); 
+ int file = open("/dev/random",O_RDONLY);
  read(file, rand, sizeof(int));
  return *rand;
 }
@@ -16,23 +16,29 @@ int main(){
  int rand[10];
  int i;
 
+printf("Generating random numbers ...\n");
  //populates and prints th array
  for(i=0;i<10;i++){
   populate(rand+i);
-  printf("rand[%d]: %d\n", i,rand[i]);
+  printf("random %d: %d\n", i,rand[i]);
  }
- 
+
  printf("\nWriting numbers to file ...\n");
- int wr = open("write.txt", O_RDWR);
- write(wr, rand, 10 * sizeof(int));
- 
+ int wr = open("write.txt",O_RDWR);
+ int bytes_written = write(wr, rand, 10 * sizeof(int));
+ //printf("writing file open: %d\n", wr);
+ //printf("bytes written: %d\n", bytes_written);
+ close(wr); // need to close and reopen to save changes
+
+ int ree = open("write.txt",O_RDONLY);
  printf("\nReading numbers to file ...\n");
  int rd [10];
- read(wr, rd, 10*sizeof(int));
- 
+ int bytes_read = read(ree, rd, 10*sizeof(int));
+ //printf("bytes read: %d\n", bytes_read);
+
   for(i=0;i<10;i++){
-  printf("rd[%d]: %d\n", i,rd[i]);
+  printf("random %d: %d\n", i,rd[i]);
  }
- 
+
  return 0;
 }
